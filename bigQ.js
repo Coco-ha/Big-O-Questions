c1eb8a6b657e83ec3392f3b1bee956abfe6900a3
Daniel Chou <daniel.cw.chou@gmail.com>
2023-07-07 15:35:19 -0400

    worked on Step Two problems

--- bigQ.js
+++ bigQ.js
@@ -8,3 +8,78 @@ O(1000 × log(n) + n)       // = O(n)
 O(1000 × n × log(n) + n)   // = O(n log(n) )
 O(2n + n2)                 // = O(n^2)
 O(5 + 3 + 1)               // = O(1)
+
+// ~~~~ Section 2 ~~~~~ //
+function logUpTo(n) {
+  for (let i = 1; i <= n; i++) {
+    console.log(i);
+  }
+}
+
+// o(n)
+
+function logAtLeast10(n) {
+  for (let i = 1; i <= Math.max(n, 10); i++) {
+    console.log(i);
+  }
+}
+
+// o(n)
+
+function logAtMost10(n) {
+  for (let i = 1; i <= Math.min(n, 10); i++) {
+    console.log(i);
+  }
+}
+
+ // o(n)
+
+function onlyNumsAtEvenIndexes(nums) {
+  let numsAtEvens = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    if (i % 2 === 0) {
+      numsAtEvens.push(nums[i]);
+    }
+  }
+
+  return numsAtEvens;
+}
+
+ // o(n)
+
+function runningSubtotals(nums) {
+  let subtotals = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    let subtotal = 0;
+    for (let j = 0; j <= i; j++) {
+      subtotal += nums[j];
+    }
+    subtotals.push(subtotal);
+  }
+
+  return subtotals;
+}
+
+//o(n^2)
+
+const VOWELS = "aeiouAEIOU";
+
+function vowelsCounts(word) {
+  let vowelsToCounts = {};
+
+  for (let char of word) {
+    if (VOWELS.includes(char)) {
+      if (char in vowelsToCounts) {
+        vowelsToCounts[char] += 1;
+      } else {
+        vowelsToCounts[char] = 1;
+      }
+    }
+  }
+
+  return vowelsToCounts;
+}
+
+// o(n)
